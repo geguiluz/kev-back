f20485442fdd440ca2290e30afcfb89d979d1c3a
geguiluz <memo.eguiluz@gmail.com>
2019-09-25 19:32:42 -0500

    Changes started for MQTT Client support (still not functional)

--- routes/deviceController.js
+++ routes/deviceController.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const mqtt = require('mqtt');
+const client = mqtt.connect('tcp://192.168.15.104');
+
 const auth = require('../middleware/auth');
 
 // @route     POST api/devices
@@ -12,7 +15,22 @@ router.patch('/toggleDevice', auth, (req, res) => {
 
   try {
     console.log('Toggling device', serialNumber);
-    return res.json({ status: 'OFF' });
+
+    client.on('connect', function() {
+      client.subscribe('presence', function(err) {
+        if (!err) {
+          client.publish('cmnd/DVES_5A55AA_fb/power', 2);
+        }
+      });
+    });
+
+    client.on('message', function(topic, payload) {
+      // message is Buffer
+      console.log(payload.toString());
+      client.end();
+    });
+
+    return res.json({ serialNumber, status: 'OFF' });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
