56138c9dd8286505b38daf34ff8f46ce25c052a6
geguiluz <memo.eguiluz@gmail.com>
2019-10-03 00:37:10 -0500

    Timer driven by environment variable

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
 
@@ -27,6 +28,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-// Sets the timer for the Home alone feature every 7 seconds. This feature can
-// be turned off by setting the first parameter to false
-scheduleTimer(true, 7);
+// The following function sets the timer for the Home Alone feature every 7
+// seconds. This feature can be turned off by setting the first parameter to
+// false
+console.log('Env: ', process.env.TIMER_ACTIVE);
+process.env.TIMER_ACTIVE === '0' ? (runTimer = false) : (runTimer = true);
+scheduleTimer(runTimer, 7);
