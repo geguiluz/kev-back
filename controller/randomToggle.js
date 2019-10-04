35ff31a37e3a0c4866f440a276a332c2aeeb689f
geguiluz <memo.eguiluz@gmail.com>
2019-10-04 02:03:32 -0500

    Performance improvements for demo purpose only

--- controller/randomToggle.js
+++ controller/randomToggle.js
@@ -1,29 +1,24 @@
 const powerDevice = require('./powerDevice');
-const getUserDevices = require('./getUserDevices');
 
-module.exports = randomToggle = async user => {
+module.exports = randomToggle = async devices => {
   try {
-    // TODO: Get the devices as an array argument
-    if (user !== null) {
-      const devices = await getUserDevices(user);
-      console.log('Toggling random devices');
-      // 1 - Map through devices passed in from the devices array
-      let devicesOff = devices.map(async currentDevice => {
-        // 2 - Generate a random interval in order to toggle actions
-        // Let's flip a coin to see if we toggle this one or not
-        let coin = Math.floor(Math.random() * 2);
-        let deviceRes = '';
-        console.log('Coin: ', coin);
-        if (coin === 1) {
-          // 3 - Call powerDevice for each serial number
-          const { serialNumber } = currentDevice;
-          console.log('Toggling ', serialNumber);
-          deviceRes = await powerDevice(serialNumber, '2');
-        }
-        return deviceRes;
-      });
-      return { msg: 'Random devices Turned off', devicesOff };
-    }
+    console.log('Toggling random devices');
+    // 1 - Map through devices passed in from the devices array
+    let devicesOff = devices.map(async currentDevice => {
+      // 2 - Generate a random interval in order to toggle actions
+      // Let's flip a coin to see if we toggle this one or not
+      let coin = Math.floor(Math.random() * 2);
+      let deviceRes = '';
+      console.log('Coin: ', coin);
+      if (coin === 1) {
+        // 3 - Call powerDevice for each serial number
+        const { serialNumber } = currentDevice;
+        console.log('Toggling ', serialNumber);
+        deviceRes = await powerDevice(serialNumber, '2');
+      }
+      return deviceRes;
+    });
+    return { msg: 'Random devices toggled', devicesOff };
   } catch (err) {
     console.error(err.message);
     return err;
