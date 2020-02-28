32d563287fb3d39f18540af7eda74b84c2947a7b
geguiluz <memo.eguiluz@gmail.com>
2020-02-27 22:30:12 -0600

    Awaiting for response (subscription) after powering device (post)

--- controller/powerDevice.js
+++ controller/powerDevice.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const mqttPost = require('./mqtt/mqttPost');
+const mqttSubscribe = require('./mqtt/mqttSubscribe');
 
 module.exports = powerDevice = async (_id, serialNumber, command) => {
   console.log(
     `${command === '2' ? `Toggling` : `Powering ${command}`} device`,
     serialNumber
   );
-  const result = await mqttPost(`cmnd/${serialNumber}_fb/power`, command);
+  await mqttPost(`cmnd/${serialNumber}_fb/power`, command);
+  const result = await mqttSubscribe(`stat/${serialNumber}_fb/POWER`);
+  console.log('Result: ', result);
   return result;
 };
